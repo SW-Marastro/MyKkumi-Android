2c0020b85bd0f6ddfca150b0a30cd76f54bdd8a2
jung0115 <jungim7490@gmail.com>
2024-05-25 10:56:02 +0900

    [BASE] #1 - NetworkModule 세팅 중 / Module 생성

--- core/data/src/main/java/com/swmarastro/mykkumi/MainActivity.kt
+++ core/data/src/main/java/com/swmarastro/mykkumi/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.swmarastro.mykkumi.android
+package com.swmarastro.mykkumi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
