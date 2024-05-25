bc9a3d8d93477e375483b821caaa3a4f4475b90a
jung0115 <jungim7490@gmail.com>
2024-05-25 15:11:45 +0900

    [CHORE] #1 - NetworkModule > 로딩 인터쳅터, retrofit 세팅 - provideRequestHttpLoggingInterceptor - provideOkHttpClien - provideRetrofit

--- core/data/src/main/java/com/swmarastro/mykkumi/data/di/NetworkModule.kt
+++ core/data/src/main/java/com/swmarastro/mykkumi/data/di/NetworkModule.kt
@@ -1,11 +1,58 @@
 package com.swmarastro.mykkumi.data.di
 
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 /*
 @Module: 인터페이스나, 빌더 패턴을 사용한 경우, 외부 라이브러리 클래스 등등 생성자를 사용할 수 없는 Class를 주입해야 할 경우
 @InstallIn: 어떤 Component에 Install할지 모듈 범위 지정. @Module이나 @EntryPoint 어노테이션과 함께 사용해야 함.
-(SingletonComponent::class): SingletonComponent에 설치한다는 뜻
+(SingletonComponent::class): 애플리케이션 전체에서 재사용 -> 네트워크 모듈은 전체에서 사용될듯하다
+@Provides: 클래스가 외부 라이브러리를 사용(Retrofit, OkHttpClient, Room databases...)하거나 빌더 패턴으로 객체 생성을 하는 경우 @Provides로 의존성 생성
 */
 @Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    // API BASE URL
+    private const val BASE_URL = ""
+
+    // 로깅인터셉터 세팅
+    @Provides
+    @Singleton
+    fun provideRequestHttpLoggingInterceptor() : HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+    // OKHttpClient에 로깅인터셉터 등록
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            //.addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
+            .build()
+    }
+
+    // API 생기면 이 부분 작성
+    /*@Provides
+    @Singleton
+    fun provideWeatherDataSource(retrofit: Retrofit): MyKkumiDataSource {
+        return retrofit.create(MyKkumiDataSource::class.java)
+    }*/
 }
