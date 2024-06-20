a44419ee4885efd78c964a9a560216484f06a7c4
jung0115 <jungim7490@gmail.com>
2024-06-20 21:03:59 +0900

    [FEAT] #14 - Bottom Navigation / home 탭 생성

--- app/src/main/java/com/swmarastro/mykkumi/android/MainActivity.kt
+++ app/src/main/java/com/swmarastro/mykkumi/android/MainActivity.kt
@@ -2,6 +2,12 @@ package com.swmarastro.mykkumi.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.swmarastro.mykkumi.android.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /*
@@ -10,8 +16,38 @@ import dagger.hilt.android.AndroidEntryPoint
 */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    // NavController 선언
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+
+        // 최상위 화면을 제외하고는 BottomNavigation Bar 없애기
+        setBottomNavigation()
+    }
+
+    // 최상위 화면을 제외하고는 BottomNavigation Bar 없애기
+    private fun setBottomNavigation() {
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            if(destination.id == com.swmarastro.mykkumi.feature.home.R.id.nav_home) {
+                binding.bottomNav.visibility = View.VISIBLE
+            } else {
+                binding.bottomNav.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
