8fe8bafaeca49c9b2722ff8cf315219420540346
jung0115 <jungim7490@gmail.com>
2024-07-10 13:43:54 +0900

    KKUMI-40 [FEAT] #36 - 카카오 로그인 버튼 배치

--- feature/auth/src/main/java/com/swmarastro/mykkumi/feature/auth/LoginComposeActivity.kt
+++ feature/auth/src/main/java/com/swmarastro/mykkumi/feature/auth/LoginComposeActivity.kt
@@ -1,16 +1,24 @@
 package com.swmarastro.mykkumi.feature.auth
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.swmarastro.mykkumi.feature.auth.ui.theme.MyKkumi_AOSTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,28 +30,40 @@ class LoginComposeActivity : ComponentActivity() {
         setContent {
             MyKkumi_AOSTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()) {
+
+                        KakaoLogin(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(30.dp)
+                                .align(Alignment.Center)
+                        )
+
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun KakaoLogin(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.kakao_login_large_wide_kor),
+        contentDescription = "kakao login btn",
         modifier = modifier
+            .fillMaxSize()
+            .pointerInteropFilter {
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        Log.d("--", "카카오 로그인 버튼")
+                    }
+                    else -> false
+                }
+                true
+            }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyKkumi_AOSTheme {
-        Greeting("Android")
-    }
 }
