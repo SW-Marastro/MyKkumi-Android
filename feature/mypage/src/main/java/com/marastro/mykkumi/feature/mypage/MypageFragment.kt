3a412099988a3f86288b4220b52bd676ebdea51d
jung0115 <jungim7490@gmail.com>
2024-10-21 21:14:05 +0900

    KKUMI-115 [FEAT] #92 - Firebase Crashlytics 연동

--- feature/mypage/src/main/java/com/marastro/mykkumi/feature/mypage/MypageFragment.kt
+++ feature/mypage/src/main/java/com/marastro/mykkumi/feature/mypage/MypageFragment.kt
@@ -3,6 +3,7 @@ package com.marastro.mykkumi.feature.mypage
 import android.content.Intent
 import android.net.Uri
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.marastro.mykkumi.common_ui.base.BaseFragment
